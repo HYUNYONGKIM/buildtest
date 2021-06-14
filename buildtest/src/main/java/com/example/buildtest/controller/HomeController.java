1096d475489dccced53918e45ef5fccbb49b576d
DESKTOP-4RR8UEB\siggr <siggraph.k@gmail.com>
2021-06-14 14:23:35 +0900

    dd

--- buildtest/src/main/java/com/example/buildtest/controller/HomeController.java
+++ buildtest/src/main/java/com/example/buildtest/controller/HomeController.java
@@ -1,13 +1,18 @@
 package com.example.buildtest.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
 	@RequestMapping("/index")
-	public String index() {
+	public String index(Model model) {
+		String a = "Hi";
+		
+		model.addAttribute("hi", a);
+		
 		return "index";
 	}
 }
